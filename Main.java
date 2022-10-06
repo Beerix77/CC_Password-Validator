f2e886fcd691559cb8c317869054afb1acf27e2a
Andrew <andrewmatysiak@hotmail.com>
2022-10-06 21:53:27 +1100

    initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter <PASSWORD>: ");
+        String password = keyboard.nextLine();
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
